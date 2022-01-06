37f14c940ca6ab188beb2536e7d3a53531745ded
Jacob Pelletier <jacobjpelletier@gmail.com>
2022-01-06 17:14:17 -0500

    do better type checking, store new images in local file system

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /* IMPORTS */
 import express from "express";
 import Resize from "./utilities/resize";
-import {promises as fs} from "fs";
+import fs from "fs";
 
 /* DEFINE KEY VARIABLES */
 // express server
@@ -9,36 +9,57 @@ const app = express();
 const port = 3000;
 
 /* SHARP */
-// endpoint: request '/' gets image
+// endpoint: request '/' gets api instructions
 app.get("/", (req: express.Request, res: express.Response) => {
+  res.send('hello');
+})
+// '/resize is API functionality
+app.get("/resize", (req: express.Request, res: express.Response, next: express.NextFunction) => {
   // Extract the query-parameter
   const widthString = req.query.width;
   const heightString = req.query.height;
+  const filename = req.query.filename;
 
-  // Parse to integer if possible
-  // set default image values - assure never undefined.
-  let width = 300;
-  let height = 300;
-  // get new width from query
-  if (widthString) {
-    width = parseInt(widthString.toString());
-  }
-  // get new height from query
-  if (heightString) {
-    height = parseInt(heightString.toString());
-  }
+  console.log(`${filename}-${widthString}-${heightString}.jpg`);
 
-  const newImg = new Resize("src/images/mario.jpg", width, height);
-  // Get the resized image
-  newImg.resize("src/images/mario.jpg", width, height).then((r) => {
-    try {
-      if (r !== undefined) {
-        r.pipe(res);
+  // if file exists, serve file
+  fs.readFile(`src/images/${filename}.jpg`, async function (err: unknown, data: unknown) {
+    // if no filename given to API, let user know
+    if (err) {
+      next(err);
+    } else {
+      // assess what data the user supplied
+      let width = 100, height = 100, file;  // set defaults to override later (or respond with err if missing)
+
+      // assess user query arguments
+      if (widthString === undefined || heightString === undefined) {
+        // inform the user that width and height arguments are missing
+        res.send('Please provide width and height')
+      } else {
+        if ('poo') {
+          res.send('Please enter numbers for dimensions')
+        } else {
+          width = parseInt(widthString.toString())
+          height = parseInt(heightString.toString())
+        }
       }
-    } catch (err) {
-      res.send(`Sorry, error occurred: ${err}`);
+
+      const newImg = new Resize(`src/images/${filename}.jpg`, width, height);
+      // Get the resized image
+      newImg.resize(`src/images/${filename}.jpg`, width, height).then((r) => {
+        try {
+          if (r !== undefined) {
+            r.pipe(res);
+          }
+        } catch (err) {
+          res.send(`Sorry, error occurred: ${err}`);
+        }
+      });
     }
-  });
+  })
+
+
+
 });
 
 app.listen(port, () => {
